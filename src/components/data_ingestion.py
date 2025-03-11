2057adec40b05fb4fa824c7edb721e7ffb576c93
elamaran25 <elamaran893@gmail.com>
2025-03-11 19:45:01 +0530

    Model Training Completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,11 +10,14 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join('artifacts',"train.csv")
-    test_data_path: str  = os.path.join('artifacts',"test.csv")
-    raw_data_path: str   = os.path.join('artifacts',"data.csv")
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
@@ -48,10 +51,11 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 if __name__=="__main__":
-    obj = DataIngestion()
-    train_data,test_data =  obj.initiate_data_ingestion()
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
